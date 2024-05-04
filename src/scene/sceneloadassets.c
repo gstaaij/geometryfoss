e9b2fb2cbc8f2139aef8f14c55ea8ac86308486c
gstaaij <gstaaij@protonmail.com>
2024-05-04 20:52:58 +0200

    Finish assets loading scene

--- src/scene/sceneloadassets.c
+++ src/scene/sceneloadassets.c
@@ -4,38 +4,164 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "ui/text.h"
+#include "scene/sceneswitcher.h"
+
+#define RESOLUTION "-uhd"
+
+static char* filesToCopy[] = {
+    "Resources/bigFont"RESOLUTION".png",
+    "Resources/bigFont"RESOLUTION".fnt",
+    "Resources/GJ_GameSheet"RESOLUTION".png",
+    "Resources/GJ_GameSheet"RESOLUTION".plist",
+};
+
+static char* fileDestinations[] = {
+    "fonts/bigFont.png",
+    "fonts/bigFont.fnt",
+    "textures/GJ_GameSheet.png",
+    "textures/GJ_GameSheet.plist",
+};
+
 SceneLoadAssets* sceneloadassetsCreate() {
     SCENE_CREATE(SceneLoadAssets);
 
+    assert(NOB_ARRAY_LEN(filesToCopy) == NOB_ARRAY_LEN(fileDestinations));
+
     this->state = SLASTATE_AWAITING_DRAG_DROP;
+
+    this->alreadyHasAssets = true;
+    for (size_t i = 0; i < NOB_ARRAY_LEN(fileDestinations); ++i) {
+        if (!FileExists(TextFormat("%s/assets/%s", GetApplicationDirectory(), fileDestinations[i]))) {
+            this->alreadyHasAssets = false;
+            break;
+        }
+    }
+
+    this->font = GetFontDefault();
 
     return this;
 }
 
 void sceneloadassetsDestroy(SceneLoadAssets* this) {
+    if (this->GDDirectory != NULL)
+        free(this->GDDirectory);
+    nob_temp_reset();
     free(this);
 }
 
 void sceneloadassetsUpdate(SceneLoadAssets* this, SceneState* sceneState, double deltaTime) {
-    (void) this;
-    (void) sceneState;
+    if (this->alreadyHasAssets) {
+        sceneswitcherTransitionEx(sceneState, SCENE_LVLED, 1.0, 0.5);
+        return;
+    }
+
+    switch (this->state) {
+        case SLASTATE_AWAITING_DRAG_DROP: {
+            if (IsFileDropped()) {
+                FilePathList filePaths = LoadDroppedFiles();
+                for (size_t i = 0; i < filePaths.count; ++i) {
+                    const char* filePath = filePaths.paths[i];
+                    if (strcmp(GetFileName(filePath), "GeometryDash.exe") == 0) {
+                        const char* dirPath = GetDirectoryPath(filePath);
+                        TraceLog(LOG_INFO, "Geometry Dash directory: %s", dirPath);
+                        this->GDDirectory = malloc(sizeof(char) * (strlen(dirPath) + 1));
+                        strcpy(this->GDDirectory, dirPath);
+                        this->state = SLASTATE_EXTRACTING_ASSETS;
+                        return;
+                    }
+                }
+            }
+        } break;
+        case SLASTATE_EXTRACTING_ASSETS: {
+            if (this->currentAssetIndex >= NOB_ARRAY_LEN(fileDestinations)) {
+                if (!sceneState->transition.transitioning)
+                    sceneswitcherTransitionTo(sceneState, SCENE_LVLED);
+                return;
+            }
+            const char* srcPath = TextFormat("%s/%s", this->GDDirectory, filesToCopy[this->currentAssetIndex]);
+            const char* destPath = TextFormat("%s/assets/%s", GetApplicationDirectory(), fileDestinations[this->currentAssetIndex]);
+            const char* assetsDir = TextFormat("%s/assets", GetApplicationDirectory());
+            if (!DirectoryExists(assetsDir)) {
+                if (!nob_mkdir_if_not_exists(assetsDir)) {
+                    this->state = SLASTATE_COPY_ERROR;
+                    this->errorMessage = nob_temp_sprintf("Couldn't create directory %s", assetsDir);
+                    return;
+                }
+            }
+            if (!DirectoryExists(GetDirectoryPath(destPath))) {
+                if (!nob_mkdir_if_not_exists(GetDirectoryPath(destPath))) {
+                    this->state = SLASTATE_COPY_ERROR;
+                    this->errorMessage = nob_temp_sprintf("Couldn't create directory %s", GetDirectoryPath(TextFormat("assets/%s", fileDestinations[this->currentAssetIndex])));
+                    return;
+                }
+            }
+            if (!nob_copy_file(srcPath, destPath)) {
+                this->state = SLASTATE_COPY_ERROR;
+                this->errorMessage = nob_temp_sprintf("Couldn't copy %s", filesToCopy[this->currentAssetIndex]);
+                return;
+            }
+
+            this->currentAssetIndex++;
+        } break;
+        default: {} break;
+    }
     (void) deltaTime;
 }
 
 void sceneloadassetsUpdateUI(SceneLoadAssets* this, SceneState* sceneState) {
-    (void) this;
+    cameraRecalculateScreenSize(&this->uiCamera);
     (void) sceneState;
 }
 
 void sceneloadassetsDraw(SceneLoadAssets* this) {
     ClearBackground(ColorFromHSV(0, 0.0, 0.25));
+    if (this->alreadyHasAssets) return;
 
     switch (this->state) {
         case SLASTATE_AWAITING_DRAG_DROP: {
-            DrawText("Drag GeometryDash.exe to this window or click here to locate it", GetScreenWidth() / 2, GetScreenHeight() / 2, 24, WHITE);
+            textDrawCentered(
+                this->uiCamera, this->font,
+                "Drag GeometryDash.exe to this window",
+                (Coord) { 0, 0 },
+                15, WHITE
+            );
         } break;
         case SLASTATE_EXTRACTING_ASSETS: {
-            assert(false && "Not implemented");
+            textDrawCentered(
+                this->uiCamera, this->font,
+                this->currentAssetIndex >= NOB_ARRAY_LEN(fileDestinations)
+                    ? "Done!"
+                    : TextFormat("Copying %s...", GetFileName(fileDestinations[this->currentAssetIndex])),
+                (Coord) { 0, 0 },
+                15, WHITE
+            );
         } break;
+        case SLASTATE_COPY_ERROR: {
+            Coord pos = { 0, 0 };
+            textDrawCentered(
+                this->uiCamera, this->font,
+                "Copying assets failed",
+                pos,
+                15, WHITE
+            );
+            pos.y -= 20;
+            if (this->errorMessage != NULL) {
+                textDrawCentered(
+                    this->uiCamera, this->font,
+                    this->errorMessage,
+                    pos,
+                    10, WHITE
+                );
+                pos.y -= 10;
+            }
+            textDrawCentered(
+                this->uiCamera, this->font,
+                "Refer to the console output for more information",
+                pos,
+                10, LIGHTGRAY
+            );
+            pos.y -= 10;
+        }
     }
 }
--- src/scene/sceneloadassets.h
+++ src/scene/sceneloadassets.h
@@ -1,13 +1,24 @@
 #pragma once
+#include <stdbool.h>
+
 #include "scene/sceneswitcher.h"
+#include "raylib.h"
+#include "camera.h"
 
 typedef enum {
     SLASTATE_AWAITING_DRAG_DROP,
     SLASTATE_EXTRACTING_ASSETS,
+    SLASTATE_COPY_ERROR,
 } SLAState;
 
 typedef struct {
     SLAState state;
+    bool alreadyHasAssets;
+    size_t currentAssetIndex;
+    char* GDDirectory;
+    const char* errorMessage;
+    Font font;
+    GDFCamera uiCamera;
 } SceneLoadAssets;
 
 SceneLoadAssets* sceneloadassetsCreate();
